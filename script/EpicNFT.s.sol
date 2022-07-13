a7b19fe67e2272d9834bf92ba7537aefc82473d3
nitegeist <nitegeist.dev@gmail.com>
2022-07-13 16:59:02 -0500

    modules

---
+++ script/EpicNFT.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import "src/EpicNFT.sol";
+
+contract ContractScript is Script {
+    EpicNFT nft;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+        nft = new EpicNFT();
+        vm.stopBroadcast();
+    }
+}
